d5ba77297a6780123edccba5835dc30b9404e2e6
hradecek <ivohradek@gmail.com>
2017-12-10 17:23:31 +0100

    Added JavaDoc

--- backend/src/main/java/com/hradecek/maps/utils/JsonUtils.java
+++ backend/src/main/java/com/hradecek/maps/utils/JsonUtils.java
@@ -4,15 +4,29 @@ import io.vertx.core.json.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 
 /**
+ * Various utility functions for {@link io.vertx.core.json.JsonObject JSON} manipulation.
+ *
+ * @see JsonObject
  * @author <a href="mailto:ivohradek@gmail.com">Ivo Hradek</a>
  */
 public class JsonUtils {
 
     /**
+     * Get value of nested key defined in <i>dotted</i> form. If no key is found null is returned.
+     * E.g. {@code some.nested.key} represents following structure with value {@code value}:
+     * <pre>
+     *   {
+     *     "some": {
+     *       "nested": {
+     *         "key": "value"
+     *       }
+     *     }
+     *   }
+     * </pre>
      *
-     * @param json
-     * @param key
-     * @return
+     * @param json JSON object to be searched
+     * @param key nested key
+     * @return value or null if no such key exists
      */
     public static String getString(JsonObject json, String key) {
         if (StringUtils.countMatches(key, ".") >= 1) {
