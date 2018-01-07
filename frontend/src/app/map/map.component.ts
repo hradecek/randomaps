12acce45ae27c0ef28cb41e3177b7513b5cfe597
hradecek <ivohradek@gmail.com>
2018-01-07 22:22:23 +0100

    Interactive route selection in sidebar

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -11,20 +11,42 @@ import { Route } from './Route';
 })
 export class MapComponent {
 
-  focusedRoute: LatLng;
+  defaultFocus = new LatLng(48.733333,  18.916667);
+
+  @Input() focusedRoute: LatLng;
   @Input() routes: Array<Route> = [];
 
   constructor(private mapService: MapService) {
-    this.focusedRoute = new LatLng(48.733333,  18.916667);
+    this.focusedRoute = this.defaultFocus;
   }
 
   addRandomRoute() {
     this.mapService.randomJourney().subscribe(encoded => {
       const decoded = EncodedPolyline.decode(encoded.points);
       const route = new Route(decoded);
+      route.color = this.getRandomColor();
 
       this.focusedRoute = route.center;
       this.routes.push(route);
     });
+  }
+
+  getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
+  refocus(index) {
+    if (this.routes.length !== 0) {
+      const next = (index === 0) ? 0 : index - 1;
+      this.focusedRoute = this.routes[next].center;
+    } else {
+      this.focusedRoute = this.defaultFocus;
+    }
   }
 }
--- frontend/src/app/route-list/route-list.component.ts
+++ frontend/src/app/route-list/route-list.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { Route } from '../map/Route';
+import {LatLng} from '../map/LatLng';
 
 @Component({
   selector: 'app-route-list',
-  templateUrl: './route-list.component.html'
+  templateUrl: './route-list.component.html',
+  styleUrls: [ './route-list.component.css' ]
 })
 export class RouteListComponent {
 
   @Input() routes: Array<Route> = [];
+  @Input() focusedRoute: LatLng;
+  @Output() removedRoute = new EventEmitter();
 
   removeRoute(event, route: Route) {
     const index = this.routes.indexOf(route, 0);
     this.routes.splice(index, 1);
+    this.removedRoute.emit(index);
+  }
+
+  selectRoute(route: Route) {
+    this.focusedRoute = route.center;
   }
 }
